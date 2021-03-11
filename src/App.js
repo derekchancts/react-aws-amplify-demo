88be9536a2432b7f502a8b14a0212f2c84dd0af0
derekchancts-at-878661647005 <derekchancts@yahoo.com.hk>
2021-03-12 06:31:03 +0800

    Added Analytics

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
+import React, {useEffect} from 'react';
 import Navbar from './pages/Navbar';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Post from './pages/Post';
 
+import {Analytics} from 'aws-amplify';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar/>
-          <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/about' component={About}/>
-            <Route path='/contact' component={Contact}/>
-            <Route path="/:post_id" component={Post} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+
+const App = () => {
+
+  useEffect(() => {
+    Analytics.record('Home page visit');
+    Analytics.record({
+      name: "UserSignIn",
+      attributes: {
+        userName: "Derek"
+      }
+    })
+  },[])
+
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/:post_id' component={Post} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
